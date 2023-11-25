09b99dc2ea5118653a21d3ed3ea71037389ca99b
Roberto Ammirata <reammirata@gmail.com>
2023-11-25 16:10:33 +0100

    anther

--- src/components/mobile/Modal.js
+++ src/components/mobile/Modal.js
@@ -5,100 +5,130 @@ import { useTranslation } from 'react-i18next';
 const MobileModal = ({ show, onClose }) => {
     const { t } = useTranslation();
     const [role, setRole] = useState('merchant');
-    const [name, setName] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
     const [submissionStatus, setSubmissionStatus] = useState(null);
 
-  
-    const googleAppsScriptEndpoint = "https://script.google.com/macros/s/AKfycbwCfvZ5tG5D5GiLJ96UExuPgsbp2xTVg7hM3V3zTyR15gRh_JbaFnacCRCXQhb23ezh/exec";
-  
+    const googleAppsScriptEndpoint = "https://script.google.com/macros/s/AKfycbwhVhquK4CsITJwCwTOqaBIwlfxFlyszKLGQzBZBxOfSH7t-yoOgbRS-_AkL3iqgKQq/exec";
+
     const handleSubmit = () => {
-        const data = { role, name, email };  
+        const data = role === 'merchant' 
+            ? { role, companyName, firstName, lastName, email, phone }
+            : { role, firstName, lastName, email, phone };
         console.log(`Sending data to ${googleAppsScriptEndpoint}:`, data);
       
         fetch(googleAppsScriptEndpoint, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),  
-          mode: 'no-cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),  
+            mode: 'no-cors',
         })
         .then((response) => {
             if (response.type === 'opaque') {
-              console.log('Sent data, but no readable response due to no-cors');
-              setSubmissionStatus("Successfully submitted! Thank you for joining the revolution.");
+                console.log('Sent data, but no readable response due to no-cors');
+                setSubmissionStatus("Successfully submitted! Thank you for joining the revolution.");
             } else {
-              console.log('Response:', response);
+                console.log('Response:', response);
             }
-          })
-          .catch((error) => {
+        })
+        .catch((error) => {
             console.log('Error:', error);
-          });
-      };
-    
-      const closeAndResetModal = () => {
+        });
+    };
+
+    const closeAndResetModal = () => {
         setRole('merchant');
-        setName('');
+        setCompanyName('');
+        setFirstName('');
+        setLastName('');
         setEmail('');
+        setPhone('');
         setSubmissionStatus('');
         onClose();
-      };
-
-      return (
-        <div style={show ? mobileModalStyles.modalOpen : mobileModalStyles.modalClose}>
-          <div style={mobileModalStyles.modalContent}>
-            <button 
-              onClick={closeAndResetModal} 
-              style={mobileModalStyles.closeButton}
-            >
-            <MdClose />
-
-            </button>
-            <h2 style={mobileModalStyles.modalTitle}>{t('Modal.title')}</h2>
-            <div style={mobileModalStyles.roleContainer}>
-              <div 
-                onClick={() => setRole('merchant')} 
-                style={role === 'merchant' ? mobileModalStyles.activeRole : mobileModalStyles.inactiveRole}
-              >
-                {t('Modal.merchantButton')}
-              </div>
-              <div 
-                onClick={() => setRole('customer')} 
-                style={role === 'customer' ? mobileModalStyles.activeRole : mobileModalStyles.inactiveRole}
-              >
-                {t('Modal.payerButton')}
-              </div>
-            </div>
-            <form>
-              <input 
-                type="text" 
-                value={name} // Added this line
-                placeholder={role === 'customer' ? t('Modal.inputPayerName') : t('Modal.inputMerchantName')} 
-                style={mobileModalStyles.inputField} 
-                onChange={(e) => setName(e.target.value)}  // Added this line
-              />
-              <input 
-                type="email" 
-                value={email} 
-                placeholder={t('Modal.inputEmail')} 
-                style={mobileModalStyles.inputField} 
-                onChange={(e) => setEmail(e.target.value)}  // Added this line
-              />
-            </form>
-            <button style={mobileModalStyles.heroButton} onClick={handleSubmit}>{t('Modal.actionButtonText')}</button>
-            <div style={mobileModalStyles.successMessageContainer}>
-            {
-            submissionStatus && 
-            <div style={mobileModalStyles.successMessage}>
-                {submissionStatus}
-            </div>
-            }
-            </div>
-          </div>
-        </div>
-      );
     };
+
+    return (
+        <div style={show ? mobileModalStyles.modalOpen : mobileModalStyles.modalClose}>
+            <div style={mobileModalStyles.modalContent}>
+                <div style={mobileModalStyles.modalHeader}>
+                    <h2 style={mobileModalStyles.modalTitle}>{t('Modal.title')}</h2>
+                    <button 
+                        onClick={closeAndResetModal} 
+                        style={mobileModalStyles.closeButton}
+                    >
+                        <MdClose />
+                    </button>
+                </div>
+                <div style={mobileModalStyles.roleContainer}>
+                    <div 
+                        onClick={() => setRole('merchant')} 
+                        style={role === 'merchant' ? mobileModalStyles.activeRole : mobileModalStyles.inactiveRole}
+                    >
+                        {t('Modal.merchantButton')}
+                    </div>
+                    <div 
+                        onClick={() => setRole('customer')} 
+                        style={role === 'customer' ? mobileModalStyles.activeRole : mobileModalStyles.inactiveRole}
+                    >
+                        {t('Modal.payerButton')}
+                    </div>
+                </div>
+                <form>
+                    {role === 'merchant' && (
+                        <input 
+                            type="text"
+                            value={companyName}
+                            placeholder={t('Modal.inputCompanyName')}
+                            style={mobileModalStyles.inputField}
+                            onChange={(e) => setCompanyName(e.target.value)}
+                        />
+                    )}
+                    <input 
+                        type="text" 
+                        value={firstName} 
+                        placeholder={t('Modal.inputFirstName')} 
+                        style={mobileModalStyles.inputField} 
+                        onChange={(e) => setFirstName(e.target.value)}  
+                    />
+                    <input 
+                        type="text" 
+                        value={lastName} 
+                        placeholder={t('Modal.inputLastName')} 
+                        style={mobileModalStyles.inputField} 
+                        onChange={(e) => setLastName(e.target.value)}  
+                    />
+                    <input 
+                        type="email" 
+                        value={email} 
+                        placeholder={t('Modal.inputEmail')} 
+                        style={mobileModalStyles.inputField} 
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="tel" 
+                        value={phone} 
+                        placeholder={t('Modal.inputPhone')} 
+                        style={mobileModalStyles.inputField} 
+                        onChange={(e) => setPhone(e.target.value)}  
+                    />
+                </form>
+                <button style={mobileModalStyles.heroButton} onClick={handleSubmit}>{t('Modal.actionButtonText')}</button>
+                <div style={mobileModalStyles.successMessageContainer}>
+                    {submissionStatus && 
+                        <div style={mobileModalStyles.successMessage}>
+                            {submissionStatus}
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const mobileModalStyles = {
   modalOpen: {
@@ -118,87 +148,102 @@ const mobileModalStyles = {
   },
   modalContent: {
     width: '90%',
-    minHeight: '82%',
+    minHeight: '80%',
     backgroundColor: 'black',
     borderRadius: '8px',
-    boxShadow: '0 0 10px rgba(255, 255, 255, 0.1)', // Updated boxShadow
+    boxShadow: '0 0 10px rgba(255, 255, 255, 0.1)',
     flexDirection: 'column',
     display: 'flex',
+    padding: '10px',
+  },
+  modalHeader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center', // This ensures vertical alignment
+    margin: '3% 15px',
   },
   modalTitle: {
-    margin: '20px',
-    fontSize: '1.5rem',
+    fontSize: '1.2rem',
+    margin: '0', // Ensuring no additional margin for the title
+    lineHeight: '1', // Adjust line height to control vertical spacing
   },
   closeButton: {
-    position: 'absolute',
-    top: '75px',  // Adjust this value to align it with your h2 title
-    right: '10px',
     background: 'none',
     border: 'none',
     color: 'white',
-    fontSize: '32px',
+    fontSize: '28px',
+    margin: '0', // Ensuring no additional margin for the button
+    lineHeight: '1', // Adjust line height to control vertical spacing
+    display: 'flex', // Using flex to align icon vertically
+    alignItems: 'center', // Aligns the close icon vertically within the button
   },
   roleContainer: {
     display: 'flex',
-    margin: '15px 30px 30px 15px',
+    margin: '10px 20px 20px 10px',
+    flexWrap: 'wrap',
   },
   activeRole: {
-    padding: '10px 20px',
-    margin: '0 10px',
+    padding: '12px 24px', // Increased padding
+    margin: '5px 10px 10px 0',
     borderRadius: '25px',
     backgroundColor: '#00D1B2',
-    width: '60px',
+    width: 'auto', // Adjust width as needed or use 'auto' for natural width
     cursor: 'pointer',
     justifyContent: 'center',
     textAlign: 'center',
     color: '#fff',
+    fontSize: '1rem', // Increased font size
   },
   inactiveRole: {
-    padding: '10px 20px',
+    padding: '12px 24px', // Increased padding
     justifyContent: 'center',
     textAlign: 'center',
-    margin: '0 10px',
-    width: '60px',
+    margin: '5px 10px 10px 0',
+    width: 'auto', // Adjust width as needed or use 'auto' for natural width
     borderRadius: '25px',
     backgroundColor: '#111111',
     cursor: 'pointer',
     color: '#fff',
+    fontSize: '1rem', // Increased font size
   },
   inputField: {
-    width: 'calc(100% - 75px)',
-    padding: '15px',
-    margin: '15px 20px',
-    fontSize: '16px',
+    width: 'calc(100% - 65px)', // Further reduced width
+    padding: '12px', // Further reduced padding
+    margin: '12px 15px', // Further reduced margin
+    fontSize: '14px', // Further reduced font size
     backgroundColor: '#000',
     color: '#fff',
     border: '2px solid #00D1B2',
-    borderRadius: '25px',
+    borderRadius: '20px', // Adjusted border radius
   },
   heroButton: {
     backgroundColor: '#00D1B2',
     color: '#fff',
     border: 'none',
-    padding: '15px 30px',
-    fontSize: '1rem',
-    borderRadius: 25,
-    margin: '30px 20px',
+    padding: '12px 25px', // Further reduced padding
+    fontSize: '0.9rem', // Further reduced font size
+    borderRadius: 20, // Adjusted border radius
+    margin: '25px 15px', // Further reduced margin
     cursor: 'pointer',
   },
   successMessageContainer: {
-    minHeight: '50px',  // sets a minimum height
+    minHeight: '40px', // Further reduced minimum height
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
   successMessage: {
-    margin: '20px',
+    margin: '15px', // Further reduced margin
     color: '#00D1B2',
-    fontSize: '16px',
-    textAlign: 'center',  // centers text horizontally
+    fontSize: '14px', // Further reduced font size
+    textAlign: 'center',
     display: 'flex',
-    justifyContent: 'center',  // centers container content horizontally
-    alignItems: 'center',  // centers container content vertically
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 };
+
+
+
 
 export default MobileModal;
--- src/components/web/Modal.js
+++ src/components/web/Modal.js
@@ -5,216 +5,231 @@ import { useTranslation } from 'react-i18next';
 const Modal = ({ show, onClose }) => {
     const { t } = useTranslation();
     const [role, setRole] = useState('merchant');
-    const [name, setName] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
     const [submissionStatus, setSubmissionStatus] = useState(null);
 
-  
-    const googleAppsScriptEndpoint = "https://script.google.com/macros/s/AKfycbwCfvZ5tG5D5GiLJ96UExuPgsbp2xTVg7hM3V3zTyR15gRh_JbaFnacCRCXQhb23ezh/exec";
-  
+    const googleAppsScriptEndpoint = "https://script.google.com/macros/s/AKfycbwhVhquK4CsITJwCwTOqaBIwlfxFlyszKLGQzBZBxOfSH7t-yoOgbRS-_AkL3iqgKQq/exec";
+
     const handleSubmit = () => {
-        const data = { role, name, email };  
+        const data = role === 'merchant' 
+            ? { role, companyName, firstName, lastName, email, phone }
+            : { role, firstName, lastName, email, phone };
         console.log(`Sending data to ${googleAppsScriptEndpoint}:`, data);
+
+        setSubmissionStatus("Submitting your information...");
+
       
         fetch(googleAppsScriptEndpoint, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),  
-          mode: 'no-cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),  
+            mode: 'no-cors',
         })
         .then((response) => {
             if (response.type === 'opaque') {
-              console.log('Sent data, but no readable response due to no-cors');
-              setSubmissionStatus("Successfully submitted! Thank you for joining the revolution.");
+                console.log('Sent data, but no readable response due to no-cors');
+                setSubmissionStatus("Successfully submitted! Thank you for joining the revolution.");
             } else {
-              console.log('Response:', response);
+                console.log('Response:', response);
             }
-          })
-          .catch((error) => {
+        })
+        .catch((error) => {
             console.log('Error:', error);
-          });
-      };
-    
-      const closeAndResetModal = () => {
+        });
+    };
+
+    const closeAndResetModal = () => {
         setRole('merchant');
-        setName('');
+        setCompanyName('');
+        setFirstName('');
+        setLastName('');
         setEmail('');
+        setPhone('');
         setSubmissionStatus('');
         onClose();
-      };
-  
-    return (
-      <div style={show ? modalStyles.modalOpen : modalStyles.modalClose}>
-        <div style={modalStyles.modalContent}>
-        <button 
-          onClick={closeAndResetModal} 
-          style={modalStyles.closeButton}
-        >
-            <MdClose />
-          </button>
-          <h2 style={modalStyles.modalTitle}>{t('Modal.title')}</h2>
-          <div style={modalStyles.roleContainer}>
-            <div 
-              onClick={() => setRole('merchant')} 
-              style={role === 'merchant' ? modalStyles.activeRole : modalStyles.inactiveRole}
-            >
-              {t('Modal.merchantButton')}
-            </div>
-            <div 
-              onClick={() => setRole('customer')} 
-              style={role === 'customer' ? modalStyles.activeRole : modalStyles.inactiveRole}
-            >
-              {t('Modal.payerButton')}
-            </div>
-          </div>
-          <form>
-            <input 
-              type="text" 
-              value={name}  // Add this line
-              placeholder={role === 'customer' ? t('Modal.inputPayerName') : t('Modal.inputMerchantName')} 
-              style={modalStyles.inputField} 
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input 
-              type="email" 
-              value={email}  // Add this line
-              placeholder={t('Modal.inputEmail')} 
-              style={modalStyles.inputField} 
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </form>
-          <button style={modalStyles.heroButton} onClick={() => {  handleSubmit(); }}>
-            {t('Modal.actionButtonText')}
-          </button>
-          {
-            submissionStatus && 
-            <div style={modalStyles.successMessage}>
-            {submissionStatus}
-            </div>
-           }
-        </div>
-      </div>
-    );
-  };
-
-  const modalStyles = {
-    modalContent: {
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        width: '30%',
-        minHeight: '80%',
-        backgroundColor: 'black',
-        borderRadius: '8px',
-        boxShadow: '0 0 10px rgba(255, 255, 255, 0.1)', // Updated boxShadow
-        
-    },
-      modalTitle: {
-        marginLeft: '40px',
-        marginTop: '35px',
-        marginBottom: '30px',
-        alignSelf: 'flex-start', // added
-        fontSize: '1.6rem',  // Increase this value as needed
-
-      },
-      modalOpen: {
-        position: 'fixed',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-        top: 0,
-        left: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.95)',  // Increased opacity
-        zIndex: 1000,
-      },
-    modalClose: {
-      display: 'none',
-    },
-    closeButton: {
-        position: 'absolute',
-        top: '35px', // Aligned with the title's top margin
-        marginRight: '15px',
-        right: '10px',
-        cursor: 'pointer',
-        background: 'none',
-        border: 'none',
-        color: 'white',
-        fontSize: '32px' // Increase the font size here if the above doesn't make it big enough
-      },
-    actionButton: {
-      backgroundColor: '#007bff',
-      color: 'white',
-      padding: '10px 20px',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-    },
-    inputField: {
-        boxSizing: 'border-box',
-        display: 'block',
-        width: 'calc(100% - 70px)', // Reduces width by 40px
-        padding: '15px',
-        marginBottom: '25px',
-        margin: '15px 40px 25px 40px', // Keeps the 40px left margin
-        fontSize: '18px',
-        backgroundColor: '#000',
-        color: '#fff',
-        border: '2px solid #00D1B2',
-        outline: 'none',
-        borderRadius: '25px',
-    },
-      heroButton: {
-        backgroundColor: '#00D1B2',
-        color: '#fff',
-        border: 'none',
-        padding: '15px 30px',
-        fontSize: '1rem',
-        borderRadius: 25,
-        margin: '30px 30px 25px 40px',
-        cursor: 'pointer',
-        transition: 'background-color 0.3s',
-      },
-      roleContainer: {
-        alignSelf: 'flex-start', // added
-        display: 'flex',
-        justifyContent: 'center',
-        margin: '15px 0px 30px 30px',
-      },
-      activeRole: {
-        padding: '15px 30px',
-        margin: '0 10px',
-        borderRadius: '25px',
-        backgroundColor: '#00D1B2',
-        cursor: 'pointer',
-        color: '#fff',
-        width: '50px', // Explicit width
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-      inactiveRole: {
-        padding: '15px 30px',
-        margin: '0 10px',
-        borderRadius: '25px',
-        backgroundColor: '#111111',
-        cursor: 'pointer',
-        color: '#fff',
-        width: '50px', // Explicit width
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-      successMessage: {
-        color: '#00D1B2',
-        textAlign: 'center',
-        marginTop: '20px',
-      },
-        
     };
+
+    return (
+        <div style={show ? modalStyles.modalOpen : modalStyles.modalClose}>
+            <div style={modalStyles.modalContent}>
+                <button 
+                    onClick={closeAndResetModal} 
+                    style={modalStyles.closeButton}
+                >
+                    <MdClose />
+                </button>
+                <h2 style={modalStyles.modalTitle}>{t('Modal.title')}</h2>
+                <div style={modalStyles.roleContainer}>
+                    <div 
+                        onClick={() => setRole('merchant')} 
+                        style={role === 'merchant' ? modalStyles.activeRole : modalStyles.inactiveRole}
+                    >
+                        {t('Modal.merchantButton')}
+                    </div>
+                    <div 
+                        onClick={() => setRole('customer')} 
+                        style={role === 'customer' ? modalStyles.activeRole : modalStyles.inactiveRole}
+                    >
+                        {t('Modal.payerButton')}
+                    </div>
+                </div>
+                <form>
+                    {role === 'merchant' && (
+                        <input 
+                            type="text"
+                            value={companyName}
+                            placeholder={t('Modal.inputCompanyName')}
+                            style={modalStyles.inputField}
+                            onChange={(e) => setCompanyName(e.target.value)}
+                        />
+                    )}
+                    <input 
+                        type="text" 
+                        value={firstName} 
+                        placeholder={t('Modal.inputFirstName')} 
+                        style={modalStyles.inputField} 
+                        onChange={(e) => setFirstName(e.target.value)}  
+                    />
+                    <input 
+                        type="text" 
+                        value={lastName} 
+                        placeholder={t('Modal.inputLastName')} 
+                        style={modalStyles.inputField} 
+                        onChange={(e) => setLastName(e.target.value)}  
+                    />
+                    <input 
+                        type="email" 
+                        value={email} 
+                        placeholder={t('Modal.inputEmail')} 
+                        style={modalStyles.inputField} 
+                        onChange={(e) => setEmail(e.target.value)}  
+                    />
+                    <input 
+                        type="tel" 
+                        value={phone} 
+                        placeholder={t('Modal.inputPhone')} 
+                        style={modalStyles.inputField} 
+                        onChange={(e) => setPhone(e.target.value)}  
+                    />
+                </form>
+                <button style={modalStyles.heroButton} onClick={handleSubmit}>
+                    {t('Modal.actionButtonText')}
+                </button>
+                {submissionStatus && 
+                    <div style={modalStyles.successMessage}>
+                        {submissionStatus}
+                    </div>
+                }
+            </div>
+        </div>
+    );
+};
+
+const modalStyles = {
+  modalContent: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    width: '29.5%', 
+    minHeight: '78%', 
+    backgroundColor: 'black',
+    borderRadius: '8px',
+    boxShadow: '0 0 10px rgba(255, 255, 255, 0.1)',
+  },
+  modalTitle: {
+    marginLeft: '39px', 
+    marginTop: '34px', 
+    marginBottom: '29px', 
+    alignSelf: 'flex-start',
+    fontSize: '1.58rem', 
+  },
+  modalOpen: {
+    position: 'fixed',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.95)',
+    zIndex: 1000,
+  },
+  modalClose: {
+    display: 'none',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: '34px', 
+    right: '10px',
+    cursor: 'pointer',
+    background: 'none',
+    border: 'none',
+    color: 'white',
+    fontSize: '31px', 
+  },
+  inputField: {
+    boxSizing: 'border-box',
+    display: 'block',
+    width: 'calc(100% - 68px)', 
+    padding: '14.5px', 
+    marginBottom: '24px', 
+    margin: '14.5px 39px', 
+    fontSize: '17.5px', 
+    backgroundColor: '#000',
+    color: '#fff',
+    border: '2px solid #00D1B2',
+    borderRadius: '25px',
+  },
+  heroButton: {
+    backgroundColor: '#00D1B2',
+    color: '#fff',
+    border: 'none',
+    padding: '14.5px 29px', 
+    fontSize: '0.98rem', 
+    borderRadius: '24.5px', 
+    margin: '29px 29px 24px 39px', 
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  },
+  roleContainer: {
+    alignSelf: 'flex-start',
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '14.5px 0px 29px 39px', 
+  },
+  activeRole: {
+    padding: '14.5px 29px', 
+    margin: '0 9.5px', 
+    borderRadius: '24.5px', 
+    backgroundColor: '#00D1B2',
+    cursor: 'pointer',
+    color: '#fff',
+  },
+  inactiveRole: {
+    padding: '14.5px 29px', 
+    margin: '0 9.5px', 
+    borderRadius: '24.5px', 
+    backgroundColor: '#111111',
+    cursor: 'pointer',
+    color: '#fff',
+  },
+  successMessage: {
+    color: '#00D1B2',
+    textAlign: 'center',
+    marginTop: '5.5px', 
+    fontSize: '0.98rem', 
+    marginBottom: '10px', // Added more bottom margin
+  },
+};
+
+
   
 
 export default Modal;
